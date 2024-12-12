6477def327ac638b33b9fcb891e18531b870e4a4
rudirynn <164328411+rudirynn@users.noreply.github.com>
2024-12-12 16:08:25 +0000

    Add files via upload
    https://www.youtube.com/watch?v=yZnmEwzHfZw timestamp 2:10:20

--- VideoTutorialHTMLCSSJS,2/app.js
+++ VideoTutorialHTMLCSSJS,2/app.js
@@ -4,6 +4,26 @@ const menuLinks = document.querySelector(".navbar__menu")
 menu.addEventListener("click", function(){
     menu.classList.toggle("is-active");
     menuLinks.classList.toggle("active");
-
-    console.log("it worked");
 });
+
+// Function to make logos move
+const logos = document.querySelector(".rotating-logos__track").cloneNode(true);
+document.querySelector(".rotating-logos").appendChild(logos);
+
+// Function to toggle the monthly yearly plan
+function switchPricing(){
+    const checkbox = document.getElementById("pricing-toggle-checkbox");
+    const monthlyPrices = document.querySelectorAll(".monthly-price");
+    const yearlyPrices = document.querySelectorAll(".yearly-price");
+    const yearlyDiscounts = document.querySelectorAll(".save-percentage");
+
+    if(checkbox.checked){
+        monthlyPrices.forEach((price) => price.classList.add("hidden"))
+        yearlyPrices.forEach((price) => price.classList.remove("hidden"))
+        yearlyDiscounts.style.display = "inline"
+    }   else{
+            monthlyPrices.forEach((price) => price.classList.remove("hidden"))
+            yearlyPrices.forEach((price) => price.classList.add("hidden"))
+            yearlyDiscounts.style.display = "none"
+    }
+}
